2f6c42cb6b8abab219a80e0c78247d43766123ea
eyobelakew <eyobelakew4@gmail.com>
2025-05-07 14:30:38 +0300

    state managment

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,10 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["Bahir Dar", "Addis Ababa", "Gondar", "Mekele", "Dire Dawa"];
-  //   Event handler
-  const handleClick = (event: MouseEvent) => console.log(event);
+  //Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   return (
     <>
       <h1>List</h1>
@@ -10,8 +12,14 @@ function ListGroup() {
       <ul className="list-group">
         {items.map((item, index) => (
           <li
-            className="list-group-item list-group-item-action"
-            onClick={handleClick}
+            className={
+              selectedIndex === index
+                ? "list-group-item list-group-item-action active"
+                : "list-group-item list-group-item-action"
+            }
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
             key={item}
           >
             {item}
